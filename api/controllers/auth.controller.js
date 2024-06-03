15b38cbb4c59e5dcb3ccc2c49d9f539986dfc219
21Average <liluno0909@gmail.com>
2024-06-03 18:46:57 +1000

    create signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,14 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.models.js';
 
-export const signup = async (req,res) =>{
+export const signup = async (req,res,next) =>{
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
     try{
         await newUser.save();
-        res.status(201).json("User Created Successfully.")
+        res.status(201).json("User Created Successfully.");
     } catch(e) {
-        res.status(500).json(e.message)
+        next(e);
     }
 };
--- api/index.js
+++ api/index.js
@@ -15,5 +15,17 @@ app.listen(3000,()=>{
     console.log('Server is running on port 3000.')
 })
 
+// Route
 app.use('/api/auth',authRouter)
+
+//middleware function for handle errors
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 
